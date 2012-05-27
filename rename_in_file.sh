cc207f54e7ba7fdd03f63104776ff619004540a8
lihex <lihe757@gmail.com>
2012-05-27 19:52:32 +0800

    add rename script

---
+++ rename_in_file.sh
@@ -0,0 +1,29 @@
+#!/bin/bash  
+#$1 change desc file
+#$2 target file
+seq()
+{
+    total=$1;
+    while [ $total -le $3 ];do
+        echo $total
+        total=$(($total+$2));
+    done;
+}
+
+words=`cat $1`;
+array=($words)
+size=${#array[@]}
+
+cp $2 $2.new
+for s in $(seq 1 2 $size)
+do
+    echo $s
+    src=${array[$(($s-1))]}
+    echo $src
+    dst=${array[$s]}
+    echo $dst
+    #echo 's/$src/$dst/g'
+    gsed -i "s/$src/$dst/" $2
+done
+
+echo "--" 
