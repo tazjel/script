58ad7e605af528262787ed47ebcbf3f1b3a04789
lihe757 <lihe757@gmail.com>
2012-09-22 14:29:50 +0800

    Ergodic directory script

---
+++ lsdir.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#使用bash做为默认shell
+list_alldir(){
+for file in $1/*
+do
+if [ -d $file ]
+then
+    echo $file
+    list_alldir $file #在这里递归调用
+els
+    #这里处理文
+fi
+done
+}
+#下面是定义初始化路径
+if [ $# -gt 0 ]
+then
+list_alldir "$1"
+else
+list_alldir "."
+fi
