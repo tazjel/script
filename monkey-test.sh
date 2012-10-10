222f0ae165ce560fd7c2dbfd3c130e990795671a
lihex <hexuan.li@cocos2d-x.org>
2012-10-10 11:42:53 +0800

    add monkey-test.sh

---
+++ monkey-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash - 
+
+MYDATE=`date +%H%M%S_%d%m%y`
+
+echo "Processing org.cocos2dx.FishingJoy2 monkey-test."
+echo "Write log in ~/monkey.$MYDATE.log"
+
+adb shell monkey -s 41341596 -p org.cocos2dx.FishingJoy2 --monitor-native-crashes --ignore-security-exceptions --kill-process-after-error --pct-trackball 0 --pct-nav 10 --pct-anyevent 10 -v -v --throttle 500 40000 > ~/monkey.$MYDATE.log
+
+echo "Monkey test stopped!"
