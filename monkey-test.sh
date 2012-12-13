da18632e2b60262a759aaa62e92bb2f7bfedb5a2
lihex <hexuan.li@cocos2d-x.org>
2012-12-13 17:23:53 +0800

    echo date after test over.

--- monkey-test.sh
+++ monkey-test.sh
@@ -8,3 +8,5 @@ echo "Write log in ~/monkey.$MYDATE.log"
 adb shell monkey -s 41341596 -p org.cocos2dx.FishingJoy2 --monitor-native-crashes --ignore-security-exceptions --kill-process-after-error --pct-trackball 0 --pct-nav 10 --pct-anyevent 10 -v -v --throttle 500 40000 > ~/monkey.$MYDATE.log
 
 echo "Monkey test stopped!"
+NOWDATE=`date +%H%M%S_%d%m%y`
+echo $NOWDATE
