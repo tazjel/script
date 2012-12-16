fe66595b0413351365224c3897a00fdb1193b40e
lihex <lihe757@gmail.com>
2012-12-16 12:43:26 +0800

    update checkChennelInfo.sh

--- checkChennelInfo_Mac.sh
+++ checkChennelInfo_Mac.sh
@@ -2,9 +2,10 @@
 
 CHENNEL_DIR=$1
 cd $CHENNEL_DIR
-if [ -e "AndroidManifest.xml" ]; then
+MANIFEST_FILE=$CHENNEL_DIR'/AndroidManifest.xml'
+if [ -e $MANIFEST_FILE ]; then
     echo
-    echo "Parsing AndroidManifest.xml"
+    echo 'Parsing '$MANIFEST_FILE
     echo "--------------------------"
 
     VERSION_CODE=`grep  android:versionCode AndroidManifest.xml \
@@ -20,25 +21,27 @@ if [ -e "AndroidManifest.xml" ]; then
     echo 'Android Debuggable: '$DEBUGABLE
     echo 'DianXin Code: '
     grep meta.*feeCode AndroidManifest.xml \
-                  | sed -E 's/.*value="(.*)".*/    \1/g'
+                  | sed -E 's/.*value=".{36}(.{8}).*".*/    \1/g'
 else
     echo "File AndroidManifest.xml is not exsit!"
 fi
 
-if [ -e "ant.properties" ]; then
+ANT_PROPERTIES=$CHENNEL_DIR'/ant.properties'
+if [ -e $ANT_PROPERTIES ]; then
     echo
-    echo "Parsing ant.properties"
+    echo 'Parsing '$ANT_PROPERTIES
     echo "--------------------------"
 
     echo `grep ^apk.channel.id   ant.properties`
     echo `grep ^apk.channel.name ant.properties`
 else
-    echo "File ant.properties is not exsit!"
+    echo 'File '$ANT_PROPERTIES' is not exsit!'
 fi
 
-if [ -e ".project" ]; then
+PROJECT_FILE=$CHENNEL_DIR'/.project'
+if [ -e $PROJECT_FILE ]; then
     echo
-    echo "Parsing .project"
+    echo 'Parsing '$PROJECT_FILE
     echo "--------------------------"
 
     PROJECT_NAME=`grep name\>Fi.*  .project \
@@ -46,10 +49,10 @@ if [ -e ".project" ]; then
         | sed -E 's/[^>]+>(.*)<.*/\1/'`
     echo 'PROJECT_NAME: '$PROJECT_NAME
 else
-    echo "File .project is not exsit!"
+    echo 'File '$PROJECT_FILE' is not exsit!'
 fi
 
-JAVA_FILE="src/org/cocos2dx/FishingJoy2/FishingJoy2.java"
+JAVA_FILE=$CHENNEL_DIR'/src/org/cocos2dx/FishingJoy2/FishingJoy2.java'
 if [ -e $JAVA_FILE ]; then
     echo
     echo 'Parsing '$JAVA_FILE
@@ -76,7 +79,7 @@ else
     echo 'File '$JAVA_FILE' is not exsit!'
 fi
 
-MODULE_FILE="jni/ModuleCustom_android.h"
+MODULE_FILE=$CHENNEL_DIR'/jni/ModuleCustom_android.h'
 if [ -e $MODULE_FILE ]; then
     echo
     echo 'Parsing '$MODULE_FILE
@@ -88,6 +91,16 @@ if [ -e $MODULE_FILE ]; then
 
 else
     echo 'File '$MODULE_FILE' is not exsit!'
+fi
+
+BUILD_NATIVE_FILE=$CHENNEL_DIR'/build_native.sh'
+if [ -e $BUILD_NATIVE_FILE ]; then
+    echo
+    echo 'Parsing '$BUILD_NATIVE_FILE
+    echo "--------------------------"
+    grep "\$GAME_AN.*copyResByResolution.*" $BUILD_NATIVE_FILE
+else
+    echo 'File '$BUILD_NATIVE_FILE' is not exsit!'
 fi
 
 # debug version
