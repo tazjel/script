f685735234d7bc61547ec2d200aba9410155fd06
lihex <hexuan.li@cocos2d-x.org>
2012-12-17 11:42:19 +0800

    update win32 version

--- checkChennelInfo.sh
+++ checkChennelInfo.sh
@@ -1,54 +1,69 @@
 #!/bin/bash
 
-CHENNEL_DIR=$1
-cd $CHENNEL_DIR
-if [ -e "AndroidManifest.xml" ]; then
+CHANNEL_DIR=$1
+CHANNEL_ID=`echo $CHANNEL_DIR \
+        | sed -r 's/.*([0-9]{6}).*/\1/' \
+        | sed -r 's/[0]*([^0]+)/\1/'`
+echo 'CHANNEL_ID: '$CHANNEL_ID
+MANIFEST_FILE=$CHANNEL_DIR'/AndroidManifest.xml'
+if [ -e $MANIFEST_FILE ]; then
     echo
-    echo "Parsing AndroidManifest.xml"
+    echo 'Parsing '$MANIFEST_FILE
     echo "--------------------------"
 
-    VERSION_CODE=`grep  android:versionCode AndroidManifest.xml \
+    VERSION_CODE=`grep  android:versionCode $MANIFEST_FILE \
                   | sed -r 's/.[^"]+"([^"]+)".*/\1/'`
-    VERSION_NAME=`grep  android:versionName AndroidManifest.xml \
+    VERSION_NAME=`grep  android:versionName $MANIFEST_FILE \
                   | sed -r 's/.[^"]+"([^"]+)".*/\1/'`
-    DEBUGABLE=`grep android:debuggable AndroidManifest.xml \
+    DEBUGABLE=`grep android:debuggable $MANIFEST_FILE \
                   | sed -r 's/.[^"]+"([^"]+)".*/\1/'`
-    CDX_CODE=`grep meta.*feeCode AndroidManifest.xml \
-                  | sed -r 's/.*value="(.*)".*/\1/g'`
     echo 'Android Version Code: '$VERSION_CODE
     echo 'Android Version Name: '$VERSION_NAME
     echo 'Android Debuggable: '$DEBUGABLE
     echo 'DianXin Code: '
-    grep meta.*feeCode AndroidManifest.xml \
-                  | sed -r 's/.*value="(.*)".*/    \1/g'
+    grep meta.*feeCode $MANIFEST_FILE \
+                  | sed -r 's/.*value=".{36}(.{8}).*".*/    \1/g'
 else
-    echo "File AndroidManifest.xml is not exsit!"
+    echo 'File '$MANIFEST_FILE' is not exsit!'
 fi
 
-if [ -e "ant.properties" ]; then
+ANT_PROPERTIES_FILE=$CHANNEL_DIR'/ant.properties'
+CHID_IN_ANT_FILE=
+if [ -e $ANT_PROPERTIES_FILE ]; then
     echo
-    echo "Parsing ant.properties"
+    echo 'Parsing '$ANT_PROPERTIES_FILE
     echo "--------------------------"
 
-    echo `grep ^apk.channel.id   ant.properties`
-    echo `grep ^apk.channel.name ant.properties`
+    echo `grep ^apk.channel.id   $ANT_PROPERTIES_FILE`
+    echo `grep ^apk.channel.name $ANT_PROPERTIES_FILE`
+    CHID_IN_ANT_FILE=`grep ^apk.channel.id $ANT_PROPERTIES_FILE \
+                 | sed -r 's/.*([0-9]{6})/\1/' \
+                 | sed -r 's/[0]*([^0]+)/\1/'`
+    echo 'CHID_IN_ANT_FILE: '$CHID_IN_ANT_FILE
 else
-    echo "File ant.properties is not exsit!"
+    echo 'File '$ANT_PROPERTIES_FILE' is not exsit!'
 fi
 
-if [ -e ".project" ]; then
+PROJECT_FILE=$CHANNEL_DIR'/.project'
+CHID_IN_PROJ_FILE=
+if [ -e $PROJECT_FILE ]; then
     echo
-    echo "Parsing .project"
+    echo 'Parsing '$PROJECT_FILE
     echo "--------------------------"
 
-    echo `grep name\>Fi.*  .project \
+    PROJECT_NAME=`grep name\>Fi.*  $PROJECT_FILE \
         | sed -r 's/\s//g' \
         | sed -r 's/[^>]+>(.*)<.*/\1/'`
+    echo 'PROJECT_NAME: '$PROJECT_NAME
+    CHID_IN_PROJ_FILE=`echo $PROJECT_NAME \
+        | sed -r 's/.*_([0-9]{6})/\1/' \
+        | sed -r 's/[0]*([^0]+)/\1/'`
+    echo 'CHID_IN_PROJ_FILE: '$CHID_IN_PROJ_FILE
 else
-    echo "File .project is not exsit!"
+    echo 'File '$PROJECT_FILE' is not exsit!'
 fi
 
-JAVA_FILE="src/org/cocos2dx/FishingJoy2/FishingJoy2.java"
+JAVA_FILE=$CHANNEL_DIR'/src/org/cocos2dx/FishingJoy2/FishingJoy2.java'
 if [ -e $JAVA_FILE ]; then
     echo
     echo 'Parsing '$JAVA_FILE
@@ -75,11 +90,54 @@ else
     echo 'File '$JAVA_FILE' is not exsit!'
 fi
 
+MODULE_FILE=$CHANNEL_DIR'/jni/ModuleCustom_android.h'
+CHID_IN_MODULE_FILE=
+if [ -e $MODULE_FILE ]; then
+    echo
+    echo 'Parsing '$MODULE_FILE
+    echo "--------------------------"
+
+    CHID_IN_MODULE_FILE=`grep "#def.*CHANNAL_ID" $MODULE_FILE \
+        | sed -r 's/[^[:digit:]]+//g'`
+    echo 'CHID_IN_MODULE_FILE: '$CHID_IN_MODULE_FILE
+
+else
+    echo 'File '$MODULE_FILE' is not exsit!'
+fi
+
+BUILD_NATIVE_FILE=$CHANNEL_DIR'/build_native.sh'
+if [ -e $BUILD_NATIVE_FILE ]; then
+    echo
+    echo 'Parsing '$BUILD_NATIVE_FILE
+    echo "--------------------------"
+    grep "\$GAME_AN.*copyResByResolution.*" $BUILD_NATIVE_FILE
+else
+    echo 'File '$BUILD_NATIVE_FILE' is not exsit!'
+fi
+
+# Genrate report
+echo
+echo "================================================="
+echo "                Report                           "
+echo "================================================="
+
+# Check all Channel id are the same
+if [[ "$CHANNEL_ID" = "$CHID_IN_ANT_FILE" && \
+      "$CHID_IN_ANT_FILE" = "$CHID_IN_PROJ_FILE" && \
+      "$CHID_IN_PROJ_FILE" = "$CHID_IN_MODULE_FILE" \
+   ]]; then
+      echo "CHANNEL_ID OK!"
+  else
+      echo 'CHANNEL ID: '$CHANNEL_ID' is not same!'
+  fi
+
+
+
 # debug version
 #echo `grep android:versionCode AndroidManifest.xml \
-#     | sed -r 's/\s//g'
+#     | sed -r 's/\s//g'`
 #echo `grep android:versionName AndroidManifest.xml \
 #     | sed -r 's/\s//g' \
-#     | sed -r 's/(.*)"an.*/\1/'`
+#     | sed -r 's/(.*)"an.*/\1/'` 
 #echo `grep android:debuggable AndroidManifest.xml | sed -r 's/\s//g'`
 
