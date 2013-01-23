5d7f5b5221a45dda07b0a140e7feba19f7c13031
lihex <hexuan.li@cocos2d-x.org>
2013-01-23 15:57:50 +0800

    add new script

---
+++ combine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+# 把这些日志FishingJoy2_000371_baoRuan_1.0.8_50.log
+# 合并输出到
+# 371.txt里面
+
+FILE_NAME=
+for f in `ls *.log`
+do
+    FILE_NAME=$FILE_NAME:`echo $f \
+        | sed -r 's/(.*)_.*/\1/' \
+        | sed -r 's/.*([0-9]{6}).*/\1/' \
+        | sed -r 's/[0]*([^0]+)/\1/' \
+        | sed -r 's/^0.*/0/'`
+done
+
+FILE_NAME=`echo $FILE_NAME | sed 's/:/_/g' | sed 's/^_//g'`
+echo $FILE_NAME
+
+for f in `ls *.log`
+do
+     cat $f |sed -E 's/Channel ID: (.*)/Channel ID: 000000\1/g' | sed -E 's/(Channel ID:.*)([0-9]{6,6})$/Channel ID: \2/g' | sed -E '/^\s/!s/(.*)/|\1/g'  | sed ':a;N;s/\n/\t/;ba;' | sed -E 's/\s//g' | sed 's/^..//g' >> $FILE_NAME'.txt'
+done
